7f05a432302a15bf18b9efe38ab0446314ebb6c3
Youssef97dev <youssefmoussafi97@gmail.com>
2022-01-08 10:58:02 +0100

    client side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [foodName, setFoodName] = useState('');
+  const [days, setDays] = useState(0);
+
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect(()=> {
+    Axios.get('http://localhost:3001/read').then((response) =>{
+      setFoodList(response.data);
+    });
+  }, []);
+
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", {foodName: foodName, days: days,});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD with Mern</h1>
+      <label>Food Name:</label>
+      <input type="text" onChange={(e) => {setFoodName(e.target.value)}} />
+      <label>Days Since You Ate It:</label>
+      <input type="number" onChange={(e) => {setDays(e.target.value)}} />
+      <button onClick={addToList}>Add To List</button>
+      <hr />
+      <h1>Food List</h1>
+      {foodList.map((val, key) => {
+        return <div key={key}><h1>{val.foodName}</h1><h1>{val.daysSinceIAte}</h1></div>
+      })}
     </div>
   );
 }
